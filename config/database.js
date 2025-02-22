c00e016f54040a5a020046e55fb26b9729ed61cc
SojibHasan <sojibsrd85@gmail.com>
2025-02-22 06:35:43 +0600

    cloudinary remove

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT');
+  const client = env("DATABASE_CLIENT", "sqlite");
 
   const connections = {
     mysql: {
