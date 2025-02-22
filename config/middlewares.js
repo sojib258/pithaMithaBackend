e7148e6b380a62e20949d0daf074d7ef596c32ea
SojibHasan <sojibsrd85@gmail.com>
2025-02-22 07:09:37 +0600

    build error fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,14 @@
-export default [
-  "strapi::logger",
-  "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
 
 // This is for cloudinary add
