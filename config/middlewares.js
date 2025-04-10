472d690bf8b7e4caf7e6310d90b509f8bc705e02
SojibHasan <sojibsrd85@gmail.com>
2025-04-10 20:11:44 +0600

    cloudinary add

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,40 +1,40 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+// module.exports = [
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
 
 // This is for cloudinary add
 
-// export default [
-//   "strapi::logger",
-//   "strapi::errors",
-//   {
-//     name: "strapi::security",
-//     config: {
-//       contentSecurityPolicy: {
-//         useDefaults: true,
-//         directives: {
-//           "connect-src": ["'self'", "https:"],
-//           "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-//           "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-//           upgradeInsecureRequests: null,
-//         },
-//       },
-//     },
-//   },
-//   "strapi::cors",
-//   "strapi::poweredBy",
-//   "strapi::query",
-//   "strapi::body",
-//   "strapi::session",
-//   "strapi::favicon",
-//   "strapi::public",
-// ];
+export default [
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
